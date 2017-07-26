62742932d9eb8b778820e5e6f4d93b5423e406e2
pdoren <pablo.saavedra@ug.uchile.cl>
2017-07-26 17:51:55 -0400

    Fixed :p

--- src/plant/ModelAssembly.h
+++ src/plant/ModelAssembly.h
@@ -76,7 +76,7 @@ public:
 	/// Gets a row.
 	/// </summary>
 	/// <returns>Returns a row constructed from this object's data.</returns>
-	Row& getRow() const;
+	Row getRow() const;
 
 	friend void to_json(json& j, const ModelAssembly& m);
 
